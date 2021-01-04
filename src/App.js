5449b5947b5e26171b1a65793831560478be8668
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-04 13:20:24 +0000

    Add styles structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./assets/styles/reset.css";
+import "./assets/styles/global.css";
+import { client } from "./client";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((response) => {
+        console.log(response);
+        setArticles(response.items);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <>
+    <div className="App">
       <h1>hello mundo</h1>
-    </>
+      <div className="container">
+        <header>
+          <div className="wrapper">
+            <span>React and Contentful</span>
+
+            {articles.map((article, index) => {
+              return <h2 key={index}>{article.fields.title}</h2>;
+            })}
+          </div>
+        </header>
+        <main>
+          <div className="wrapper"></div>
+        </main>
+      </div>
+    </div>
   );
 }
 
