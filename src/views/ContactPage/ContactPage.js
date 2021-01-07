2c3e749c45828c1a80468bc8d678cad981f662ad
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-07 17:43:21 +0000

    Add a modal as a feedback for contact

--- src/views/ContactPage/ContactPage.js
+++ src/views/ContactPage/ContactPage.js
@@ -1,40 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
+import Hero from "../../assets/images/contact-dog.jpg";
+
 import "./ContactPage.css";
 
-function sendEmail(e) {
-  e.preventDefault();
-  console.log("Sending form as an e-mail...");
-
-  emailjs
-    .sendForm(
-      process.env.REACT_APP_EMAILJS_SERVICE_ID,
-      "template_zlpkl96",
-      e.target,
-      process.env.REACT_APP_EMAILJS_USER_ID
-    )
-    .then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    )
-    .then(e.target.reset());
-}
-
 function ContactPage() {
+  const [showModal, setShowModal] = useState(true);
+
+  function sendEmail(e) {
+    e.preventDefault();
+    console.log("Sending form as an e-mail...");
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        "template_zlpkl96",
+        e.target,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      )
+      .then(() => {
+        e.target.reset();
+        setShowModal(true);
+      });
+  }
+
   return (
-    <div className="App">
+    <div className="App full-screen">
       <Navbar />
 
       <div className="container contact-container">
-        <div className="hero-image"></div>
+        <div
+          className="hero-image"
+          // styles={{ backgroundImage: `url(${Hero})` }}
+        >
+          {" "}
+        </div>
         <div className="form-wrapper">
           <form className="contact-form" onSubmit={sendEmail}>
             <h1>Contact Us </h1>
@@ -65,6 +77,21 @@ function ContactPage() {
       </div>
 
       <Footer />
+
+      <div
+        className={`contact-modal ${showModal ? "" : "hidden"}`}
+        onClick={() => setShowModal(false)}
+      >
+        <div className="modal-content">
+          Thank you for getting in touch!
+          <span
+            className="close-modal noselect"
+            onClick={() => setShowModal(false)}
+          >
+            X
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
